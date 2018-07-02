d8499c63bee86b8aaeb0483d59a9323a4a34dcb1
Ashwin <aswinjeganath@gmail.com>
2018-07-03 03:00:28 +0530

    Set up sockets client side

--- socket-server.js
+++ socket-server.js
@@ -1,4 +1,5 @@
 const socketIOJwt = require('socketio-jwt');
+const User = require('./models/user');
 const users = {};
 function socketServer(io) {
   io.sockets
@@ -13,20 +14,21 @@ function socketServer(io) {
       // All socket events go here
       // Identify the user associated with this socket
       try {
-        let user = User.findById(socket.decoded_token.id);
+        let user = await User.findById(socket.decoded_token.id);
+        console.log(user);
         socket.username = user.username;
         console.log(
           `${socket.username} has connected through socket ${socket.id}`
         );
-        console.log(users);
 
-        if (!users.hasOwnPropert(socket.username)) users[socket.username] = {};
+        if (!users.hasOwnProperty(socket.username)) users[socket.username] = {};
 
         if (users[socket.username].sockets) {
           users[socket.username].sockets.push(socket.id);
         } else {
           users[socket.username].sockets = [socket.id];
         }
+        console.log(users);
       } catch (err) {
         console.log(err);
       }
